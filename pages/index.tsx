c03a7ebf5c4a6d2e1a7de45b2de002d49580e445
Moisés Lopes Ferreira <terced.moisesf@ftd.com.br>
2021-08-12 23:54:14 -0300

    Add init files

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+      <iframe src="https://olhohumano.sounder.fm/" style={{position:'fixed', top:0, left:0, bottom:0, right:0, width:'100%', height:'100%', border:'none', margin:0, padding:0, overflow:'hidden', zIndex:999999,}}/>
+  );
+};
+
+export default Index;
